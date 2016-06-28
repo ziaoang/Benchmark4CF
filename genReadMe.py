c1f16e7fcfe6d3c1af4802fc493906fc64893604
ziaoang <ziaoang@gmail.com>
2016-06-28 14:17:24 +0900

    add svdfeature

--- genReadMe.py
+++ genReadMe.py
@@ -3,13 +3,13 @@ import numpy as np
 
 rmse_dict = defaultdict(lambda:defaultdict(list))
 mae_dict = defaultdict(lambda:defaultdict(list))
-for filename in ["rule", "libmf", "libfm"]:
+for filename in ["rule", "libmf", "libfm", "svdfeature"]:
     for line in open("src/log/%s.log"%filename):
         method, dataset, cross, rmse, mae = line.strip().split("\t")
         rmse_dict[method][dataset].append(float(rmse))
         mae_dict[method][dataset].append(float(mae))
 
-method_list = ["global mean", "user mean", "item mean", "user item mean", "libmf", "libfm sgd", "libfm als", "libfm mcmc"]
+method_list = ["global mean", "user mean", "item mean", "user item mean", "libmf", "libfm sgd", "libfm als", "libfm mcmc", "svdfeature"]
 dataset_list = ["ml-100k", "ml-1m", "ml-10m", "ml-20m", "netflix"]
 
 out = ""
